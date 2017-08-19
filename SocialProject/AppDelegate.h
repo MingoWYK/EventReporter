1182c613f81bc2b65706e9f9ec8ce09eba3e98a1
Yuakai Weng <wengyuankai@mingos-mbp.wv.cc.cmu.edu>
2017-08-19 13:46:47 -0400

    Initial Commit

---
+++ SocialProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocialProject
+//
+//  Created by wengyuankai on 8/19/17.
+//  Copyright © 2017 wengyuankai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocialProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SocialProject
+//
+//  Created by wengyuankai on 8/19/17.
+//  Copyright © 2017 wengyuankai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
